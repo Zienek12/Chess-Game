dcdc7391def6708b6296e4abc7230e0eec4547e0
Zienek <280147@student.pwr.edu.pl>
2025-05-23 00:55:58 +0200

    created files

---
+++ Chess/Pieces/Pawn/Pawn.cpp
@@ -0,0 +1,9 @@
+#include "Pawn.h"
+std::vector<Position> Pawn::getLegalMoves(const Position& pos, const Board& board)
+{
+	std::vector<Position> moves;
+	Position oneForward = Position(pos.x, pos.y + direction);
+
+
+	return moves;
+}
---
+++ Chess/Pieces/Pawn/Pawn.h
@@ -0,0 +1,9 @@
+#include "../Piece.h"
+#include "../Position.h"
+#include "../../Board/Board.h"
+class Pawn : public Piece
+{
+public:
+	Pawn(Color color, bool hasMoved = false) : Piece(PieceType::Pawn, color, hasMoved) {}
+	std::vector<Position> getLegalMoves(const Position& pos, const Board& board);
+};
