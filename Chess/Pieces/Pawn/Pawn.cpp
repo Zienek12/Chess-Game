fb931d3b05dcab05eada0ecde204b857566277d5
Zienek <280147@student.pwr.edu.pl>
2025-05-23 01:53:12 +0200

    added basic pawn functionality moving forward and attacking NYI: -en passante -pawn promotion

--- Chess/Pieces/Pawn/Pawn.cpp
+++ Chess/Pieces/Pawn/Pawn.cpp
@@ -1,9 +1,34 @@
 #include "Pawn.h"
+#include <utility>
+
 std::vector<Position> Pawn::getLegalMoves(const Position& pos, const Board& board)
 {
-	std::vector<Position> moves;
-	Position oneForward = Position(pos.x, pos.y + direction);
+    int direction = (getColor() == Color::White) ? 1 : -1;
+    int startRow = (getColor() == Color::White) ? 1 : 6;
 
+    std::vector<Position> moves;
+    Position oneForward = Position(pos.x, pos.y + direction);
+    if (oneForward.isValid() && board.getPiece(oneForward).getType() == PieceType::None)
+    {
+        moves.push_back(oneForward);
+        Position twoForward = Position(pos.x, pos.y + direction * 2);
+        if (pos.y == startRow && twoForward.isValid() && board.getPiece(twoForward).getType() == PieceType::None)
+        {
+            moves.push_back(twoForward);
+        }
+    }
+    for (int dx : {-1, 1})
+    {
+        Position diag(pos.x + dx, pos.y + direction);
+        if (diag.isValid())
+        {
+            const Piece& target = board.getPiece(diag);
+            if (target.getType() != PieceType::None && target.getColor() != getColor() && target.getColor() != Color::None)
+            {
+                moves.push_back(diag);
+            }
+        }
+    }
 
-	return moves;
+    return moves;
 }
