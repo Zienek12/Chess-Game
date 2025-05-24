958283f14b2468dfaee570a14b2d90d752712551
Zienek <280147@student.pwr.edu.pl>
2025-05-24 20:31:44 +0200

    added checking if move is correctly inputed to avoid crashing

--- Chess/HandlePlayerInput/HandlePlayerInput.cpp
+++ Chess/HandlePlayerInput/HandlePlayerInput.cpp
@@ -4,11 +4,24 @@
 #include "../Pieces/Position.h"
 PlayerMove HandlePlayerInput::getPlayerMove() const
 {
-    std::string input;
-    std::cout << "Podaj ruch (np. e2 e4): ";
     PlayerMove move;
-    std::cin >> move.from >> move.to;
+    while (true)
+    {
+        std::cout << "Podaj ruch (np. e2 e4): ";
+        std::cin >> move.from >> move.to;
 
+        if (move.from.size() == 2 && move.to.size() == 2 &&
+            move.from[0] >= 'a' && move.from[0] <= 'h' &&
+            move.from[1] >= '1' && move.from[1] <= '8' &&
+            move.to[0] >= 'a' && move.to[0] <= 'h' &&
+            move.to[1] >= '1' && move.to[1] <= '8')
+        {
+            break;
+        }
+        std::cout << "Incorrect move format! Please try again\n";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
     return move;
 }
 
