71793acf3945756502a5c6a5d61352d609059010
Zienek <280147@student.pwr.edu.pl>
2025-05-28 12:47:49 +0200

    added base constructor

--- Chess/Pieces/Move.h
+++ Chess/Pieces/Move.h
@@ -6,7 +6,8 @@ struct Move {
     Position from;
     Position to;
     PieceType promotionType = PieceType::None;
-
+    
+    Move() : from(Position()), to(Position()), promotionType(PieceType::None) {}
     Move(const Position& from, const Position& to, PieceType promotionType = PieceType::None)
         : from(from), to(to), promotionType(promotionType) {
     }
